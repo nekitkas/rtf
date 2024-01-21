e05c927b82e56fcfaf7ed73e271ed5ce4ecb31b5
yahve0110 <ilik124darc@gmail.com>
2024-01-21 18:03:04 +0200

    webpack node server

---
+++ client_main/src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import "../../styles/components_styles/navbar.css";
+import searchSvg from "../../assets/img/search.svg";
+import plusSvg from "../../assets/img/plus.svg";
+import avatar from "../../assets/img/avatar.svg.png";
+import arrowSvg from "../../assets/img/arrow.svg";
+import messageButtonImage from "../../assets/img/message.svg";
+
+export function Navbar() {
+  const mainContainer = document.querySelector(".root");
+  mainContainer.innerHTML = `
+    <nav class="navbar">
+
+    <a href="#/home"> <h1 class="logo">VOYAGE</h1></a>
+    </div>
+    <div class="navbar-auth">
+      
+        <a href="#/login"><div class="navbar-button">Sign in</div></a>
+      
+        <a href="#/register">
+        <div class="navbar-button"><p>Sign up</p></div>
+      </a>
+      </div>
+  </nav>`;
+}
