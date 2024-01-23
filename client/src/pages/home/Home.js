404aa057e792641360c8d5464af7e46c27cd76b1
Zindiks <103096207+Zindiks@users.noreply.github.com>
2024-01-23 04:06:54 +0200

    REAL-25 PostFeed + Post Component

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,12 +1,37 @@
 import { Navbar } from "../../components/Navbar/Navbar.js";
 import "../../styles/style.css"
+import "../../styles/post.css"
+import {RenderPost} from "../../components/Post";
+import {PostDataTest} from "../../components/postDataTest";
+
+export const Container = document.createElement("div")
+export const PostFeed = document.createElement("div")
+PostFeed.classList.add("post-feed")
+
+
+
+
+
+
+
 
 export function RenderHomePage(){
    const rootContainer = document.querySelector('.root');
     Navbar()
-    const mainContainer = document.createElement("div")
-    mainContainer.classList.add("container")
-    rootContainer.append(mainContainer)
+
+
+    Container.classList.add("container")
+    rootContainer.append(Container)
+
+    const post = RenderPost(PostDataTest)
+ const post2 = RenderPost(PostDataTest)
+
+    PostFeed.appendChild(post)
+ PostFeed.appendChild(post2)
+
+
+ Container.appendChild(PostFeed)
+
     
 
 
