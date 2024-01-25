b4333616b211db98d71f8a0f375b3e256f0ee96f
yahve0110 <ilik124darc@gmail.com>
2024-01-25 19:54:59 +0200

    Real24 Post

--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -116,7 +116,7 @@ export async function RenderRegisterPage() {
     })
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Network response was not ok")
+          throw new Error("Network response was not ok",error)
 
         }
         return response.json()
